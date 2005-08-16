86fcab31846b19c6e5302e6e2bca58fe854ee9fc
godunko <godunko@661adc5f-87f8-0310-85d6-9a7b5f58d823>
2005-08-16 10:19:00 +0000

    Добавлены заголовки фалов связки с Xt.

--- xmada/trunk/source/xt/xt-translation_management.adb
+++ xmada/trunk/source/xt/xt-translation_management.adb
@@ -1,10 +1,47 @@
-with Interfaces.C;      use Interfaces.C;
-
-with Xt.Implementation; use Xt.Implementation;
-
+------------------------------------------------------------------------------
+--
+--  X Toolkit Intrinsics Ada Binding
+--
+------------------------------------------------------------------------------
+--! <Copyright>
+--!  Copyright (C) 2004-2005  Vadim Godunko (vgodunko@rost.ru)
+--!
+--! XmAda is free software; you can redistribute it and/or modify it under
+--! the terms of the GNU General Public License as published by the Free
+--! Software Foundation; either version 2, or (at your option) any later
+--! version.
+--!
+--! XmAda is distributed in the hope that it will be useful, but WITHOUT ANY
+--! WARRANTY; without even the implied warranty of MERCHANTABILITY or
+--! FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+--! for more details.
+--!
+--! You should have received a copy of the GNU General Public License
+--! along with GCC; see the file COPYING. If not, write to the Free
+--! Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
+--! 02110-1301, USA.
+--!
+--! As a special exception, if other files instantiate generics from this
+--! unit, or you link this unit with other files to produce an executable,
+--! this unit does not by itself cause the resulting executable to be
+--! covered by the GNU General Public License. This exception does not
+--! however invalidate any other reasons why the executable file might be
+--! covered by the GNU Public License.
+--!
+--! <Unit> Xt.Translation_Management
+--! <ImplementationNotes>
+--! <PortabilityIssues>
+--! <AnticipatedChanges>
+------------------------------------------------------------------------------
+--  $Revision$ $Author$
+--  $Date$
+------------------------------------------------------------------------------
+with Xt.Implementation;
 
 package body Xt.Translation_Management is
 
+   use Interfaces.C;
+   use Xt.Implementation;
 
    --  !!! оригинальная реализация делает ещё не ясно что !!!
    procedure Xt_App_Add_Actions (App_Context : in Xt_App_Context;
