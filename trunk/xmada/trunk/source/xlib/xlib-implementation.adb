2053198c78ea5a3b5a886c286b51c072b8201549
godunko <godunko@661adc5f-87f8-0310-85d6-9a7b5f58d823>
2005-08-16 07:54:07 +0000

    Добавлены заголовки файлов для связки с Xlib.

--- trunk/xmada/trunk/source/xlib/xlib-implementation.adb
+++ trunk/xmada/trunk/source/xlib/xlib-implementation.adb
@@ -1,12 +1,51 @@
---  !!! This module is GNAT specific. It use unchecked conversion between !!!
---  !!! general access type and address and address arithmetic. !!!
+------------------------------------------------------------------------------
+--
+--  X Library Ada Binding
+--
+------------------------------------------------------------------------------
+--! <Copyright>
+--!  Copyright (C) 2004-2005  Vadim Godunko (vgodunko@rost.ru)
+--!
+--! XmAda is free software; you can redistribute it and/or modify it under
+--! the terms of the GNU General Public License as published by the Free
+--! Software Foundation; either version 2, or (at your option) any later
+--! version.
+--!
+--! XmAda is distributed in the hope that it will be useful, but WITHOUT ANY
+--! WARRANTY; without even the implied warranty of MERCHANTABILITY or
+--! FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+--! for more details.
+--!
+--! You should have received a copy of the GNU General Public License
+--! along with GCC; see the file COPYING. If not, write to the Free
+--! Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
+--! 02110-1301, USA.
+--!
+--! As a special exception, if other files instantiate generics from this
+--! unit, or you link this unit with other files to produce an executable,
+--! this unit does not by itself cause the resulting executable to be
+--! covered by the GNU General Public License. This exception does not
+--! however invalidate any other reasons why the executable file might be
+--! covered by the GNU Public License.
+--!
+--! <Unit> Xlib.Implementation
+--! <ImplementationNotes>
+--! <PortabilityIssues>
+--!   This module is GNAT specific. It use unchecked conversion between
+--! general access type and address and address arithmetic.
+--!
+--! <AnticipatedChanges>
+------------------------------------------------------------------------------
+--  $Revision$ $Author$
+--  $Date$
+------------------------------------------------------------------------------
 with Ada.Unchecked_Conversion;
-with System;                  use System;
-with System.Storage_Elements; use System.Storage_Elements;
-
+with System.Storage_Elements;
 
 package body Xlib.Implementation is
 
+   use System;
+   use System.Storage_Elements;
 
    procedure Check (The_Display : in Display) is
    begin
