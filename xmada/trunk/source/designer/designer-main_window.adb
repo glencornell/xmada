9c1eb6e061bc19379d4de8b05ed6059f67a703b0
godunko <godunko@661adc5f-87f8-0310-85d6-9a7b5f58d823>
2006-01-27 10:51:03 +0000

    Добавлены заготовки пакетов реализации дизайнера.

---
+++ xmada/trunk/source/designer/designer-main_window.adb
@@ -0,0 +1,57 @@
+------------------------------------------------------------------------------
+--
+--  XmAda Designer
+--
+------------------------------------------------------------------------------
+--! <Copyright>
+--!  Copyright (C) 2006
+--!
+--! XmAda is free software; you can redistribute it and/or modify it under
+--! the terms of the GNU General Public License as published by the Free
+--! Software Foundation; either version 2, or (at your option) any later
+--! version.
+--!
+--! XmAda is distributed in the hope that it will be useful, but WITHOUT ANY
+--! WARRANTY; without even the implied warranty of MERCHANTABILITY or
+--! FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+--! for more details.
+--!
+--! You should have received a copy of the GNU General Public License
+--! along with XmAda; see the file COPYING. If not, write to the Free
+--! Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+--! USA.
+--!
+--! As a special exception, if other files instantiate generics from this
+--! unit, or you link this unit with other files to produce an executable,
+--! this unit does not by itself cause the resulting executable to be
+--! covered by the GNU General Public License. This exception does not
+--! however invalidate any other reasons why the executable file might be
+--! covered by the GNU Public License.
+--!
+--! <Unit> Designer.Main_Window
+--! <ImplementationNotes>
+--! <PortabilityIssues>
+--! <AnticipatedChanges>
+------------------------------------------------------------------------------
+--  $Revision$ $Author$
+--  $Date$
+------------------------------------------------------------------------------
+with Designer.Properties_Editor;
+with Designer.Tree_Editor;
+with Designer.Visual_Editor;
+
+package body Designer.Main_Window is
+
+   ---------------------------------------------------------------------------
+   --! <Subprogram>
+   --!    <Unit> Initialize
+   --!    <ImplementationNotes>
+   ---------------------------------------------------------------------------
+   procedure Initialize is
+   begin
+      Designer.Properties_Editor.Initialize;
+      Designer.Tree_Editor.Initialize;
+      Designer.Visual_Editor.Initialize;
+   end Initialize;
+
+end Designer.Main_Window;
