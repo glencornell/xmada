4ab330a0b4c8742a52e66b080699d6da088f0884
dibala <dibala@661adc5f-87f8-0310-85d6-9a7b5f58d823>
2006-01-28 08:02:42 +0000

    небольшие изменения в инициализации

--- trunk/xmada/trunk/source/designer/designer-properties_editor.adb
+++ trunk/xmada/trunk/source/designer/designer-properties_editor.adb
@@ -44,7 +44,7 @@ package body Designer.Properties_Editor is
    --!    <Unit> Initialize
    --!    <ImplementationNotes>
    ---------------------------------------------------------------------------
-   procedure Initialize is
+   procedure Initialize (Parent : in Xt.Widget) is
    begin
       null;
    end Initialize;
