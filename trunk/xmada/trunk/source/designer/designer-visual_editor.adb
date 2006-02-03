469cf5bd686cfc310786383554e401d34340ce0d
godunko <godunko@661adc5f-87f8-0310-85d6-9a7b5f58d823>
2006-02-03 07:47:12 +0000

    При добавлении проекта производится установка конверторов обратного преобразования для всех перечислимых ресурсных типов.

--- trunk/xmada/trunk/source/designer/designer-visual_editor.adb
+++ trunk/xmada/trunk/source/designer/designer-visual_editor.adb
@@ -36,14 +36,39 @@
 --  $Revision$ $Author$
 --  $Date$
 ------------------------------------------------------------------------------
+with Ada.Characters.Handling;
+
+with Xm.Representation_Type_Management;
 with Xm_Drawing_Area;
 with Xm_Scrolled_Window;
 
+with Designer.Main_Window;
+with Model.Queries;
+with Model.Tree.Lists;
+
 package body Designer.Visual_Editor is
 
+   use Model;
+   use Model.Queries;
+   use Model.Tree;
+   use Model.Tree.Lists;
+   use Xm.Representation_Type_Management;
    use Xm_Drawing_Area;
    use Xm_Scrolled_Window;
    use Xt;
+
+   ---------------------------------------------------------------------------
+   --! <Subprogram>
+   --!    <Unit> Setup_Reverse_Converters
+   --!    <Purpose> Производит установку конверторов обратного преобразования
+   --! для всех известных перечислимых типов ресурсов.
+   --!    <Exceptions>
+   ---------------------------------------------------------------------------
+   procedure Setup_Reverse_Converters (Types : in List_Id);
+   --  XXX В последующем эту подпрограмму необходимо вынести в отдельный
+   --  пакет, который будет производить всю начальную инициализацию и
+   --  проверку соответствия загруженной модели реально поддерживаемым
+   --  возможностям используемой библиотеки времени исполнения.
 
    ---------------------------------------------------------------------------
    --! <Subprogram>
@@ -73,6 +98,7 @@ package body Designer.Visual_Editor is
    procedure Initialize (Parent : in Xt.Widget) is
       Scroll : Widget;
       Draw   : Widget;
+
    begin
       Scroll := Xm_Create_Managed_Scrolled_Window (Parent, "scrolled");
       Draw   := Xm_Create_Managed_Drawing_Area (Scroll, "drawing");
@@ -85,7 +111,31 @@ package body Designer.Visual_Editor is
    ---------------------------------------------------------------------------
    procedure Insert_Item (Node : in Model.Node_Id) is
    begin
-      null;
+      case Node_Kind (Node) is
+         when Node_Project =>
+            --  Устанавливаем обратные конверторы типов ресурсов.
+            declare
+               Set : Node_Id := First (Imported_Widget_Sets (Node));
+
+            begin
+               while Set /= Null_Node loop
+                  Setup_Reverse_Converters (Resource_Types (Set));
+                  Set := Next (Set);
+               end loop;
+            end;
+
+         when Node_Application =>
+            null;
+
+         when Node_Component_Class =>
+            null;
+
+         when Node_Widget_Instance =>
+            null;
+
+         when others =>
+            raise Program_Error;
+      end case;
    end Insert_Item;
 
    ---------------------------------------------------------------------------
@@ -117,6 +167,43 @@ package body Designer.Visual_Editor is
    begin
       null;
    end Set_Properties;
+
+   ---------------------------------------------------------------------------
+   --! <Subprogram>
+   --!    <Unit> Setup_Reverse_Converters
+   --!    <ImplementationNotes>
+   ---------------------------------------------------------------------------
+   procedure Setup_Reverse_Converters (Types : in List_Id) is
+      Aux : Node_Id := First (Types);
+      Id  : Xm_Rep_Type_Id;
+
+   begin
+      while Aux /= Null_Node loop
+         if Node_Kind (Aux) = Node_Enumerated_Resource_Type then
+            begin
+               Id :=
+                 Xm_Rep_Type_Get_Id
+                  (Ada.Characters.Handling.To_String
+                    (Internal_Name_Image (Aux)));
+               Xm_Rep_Type_Add_Reverse (Id);
+
+               --  XXX Кроме добавления обратных конверторов здесь желательно
+               --  проверить соответствие состава литералов. Но это уже после
+               --  создания специального модуля проверки соответствия модели
+               --  и среды выполнения.
+
+            exception
+               when Unknown_Representation_Type_Error =>
+                  Designer.Main_Window.Put_Line
+                   ("Representation type "
+                      & Internal_Name_Image (Aux)
+                      & " is unknown");
+            end;
+         end if;
+
+         Aux := Next (Aux);
+      end loop;
+   end Setup_Reverse_Converters;
 
    ---------------------------------------------------------------------------
    --! <Subprogram>
