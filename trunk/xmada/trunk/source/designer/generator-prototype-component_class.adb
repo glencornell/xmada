9d4e6953bda0ba9ef82ce46d3f8778f3d0d38782
marchenko <marchenko@661adc5f-87f8-0310-85d6-9a7b5f58d823>
2006-02-14 06:02:44 +0000

    * trunk/source/designer/generator-prototype-component_class.adb
    Исправления стиля.

--- trunk/xmada/trunk/source/designer/generator-prototype-component_class.adb
+++ trunk/xmada/trunk/source/designer/generator-prototype-component_class.adb
@@ -49,113 +49,115 @@ package body Generator.Prototype.Component_Class is
    ---------------------------------------------------------------------------
    procedure Generate (Node : in Model.Node_Id) is
 
-      procedure Generate_Package (File : File_Type; 
+      procedure Generate_Package (File : File_Type;
                                   Package_Name : Wide_String);
 
-      procedure Generate_Package (File : File_Type; 
+      procedure Generate_Package (File : File_Type;
                                   Package_Name : Wide_String)
-      is                                 
+      is
       begin
    --    generation of spec
 
-         Put_Line(File => File,
-                  Item => "package " & Package_Name & "s is");
+         Put_Line (File => File,
+                   Item => "package " & Package_Name & "s is");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "   type " & Package_Name & " is limited private;");
+         Put_Line (File => File,
+                   Item => "   type " & Package_Name & " is limited private;");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "   type " & Package_Name & "_Access is access all " &
-                           Package_Name & ";");
+         Put_Line (File => File,
+                   Item => "   type " & Package_Name & "_Access is access all "
+                           & Package_Name & ";");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "   package Constructors is");
+         Put_Line (File => File,
+                   Item => "   package Constructors is");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "      procedure Initialize (Object : access " &
-                           Package_Name & ";");
-         Put_Line(File => File,
-                  Item => "                            Parent : in Xt.Widget);");
+         Put_Line (File => File,
+                   Item => "      procedure Initialize (Object : access "
+                           & Package_Name & ";");
+         Put_Line
+          (File => File,
+           Item => "                            Parent : in Xt.Widget);");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "      function Create (Parent : in Xt.Widget)");
-         Put_Line(File => File,
-                  Item => "        return " & Package_Name & "_Access;");
+         Put_Line (File => File,
+                   Item => "      function Create (Parent : in Xt.Widget)");
+         Put_Line (File => File,
+                   Item => "        return " & Package_Name & "_Access;");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "   end Constructors;");
+         Put_Line (File => File,
+                   Item => "   end Constructors;");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "private");
+         Put_Line (File => File,
+                   Item => "private");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "   type " & Package_Name & " is limited record");
-         Put_Line(File => File,
-                  Item => "      null;");
-         Put_Line(File => File,
-                  Item => "   end record;");
-         Put_Line(File => File,
-                  Item => "end " & Package_Name & "s;");
+         Put_Line (File => File,
+                   Item => "   type " & Package_Name & " is limited record");
+         Put_Line (File => File,
+                   Item => "      null;");
+         Put_Line (File => File,
+                   Item => "   end record;");
+         Put_Line (File => File,
+                   Item => "end " & Package_Name & "s;");
 
          New_Line (File);
    --    generation of body
 
-         Put_Line(File => File,
-                  Item => "package body " & Package_Name & "s is");
+         Put_Line (File => File,
+                   Item => "package body " & Package_Name & "s is");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "   package CallBacks is");
-         Put_Line(File => File,
-                  Item => "      null;");
-         Put_Line(File => File,
-                  Item => "   end CallBacks;");
+         Put_Line (File => File,
+                   Item => "   package CallBacks is");
+         Put_Line (File => File,
+                   Item => "      null;");
+         Put_Line (File => File,
+                   Item => "   end CallBacks;");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "   package body CallBacks is");
-         Put_Line(File => File,
-                  Item => "      null;");
-         Put_Line(File => File,
-                  Item => "   end CallBacks;");
+         Put_Line (File => File,
+                   Item => "   package body CallBacks is");
+         Put_Line (File => File,
+                   Item => "      null;");
+         Put_Line (File => File,
+                   Item => "   end CallBacks;");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "   package body Constructors is");
+         Put_Line (File => File,
+                   Item => "   package body Constructors is");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "      procedure Initialize (Object : access " &
-                           Package_Name & ";");
-         Put_Line(File => File,
-                  Item => "                            Parent : in Xt.Widget)");
-         Put_Line(File => File,
-                  Item => "      is");
-         Put_Line(File => File,
-                  Item => "      begin");
-         Put_Line(File => File,
-                  Item => "         null;");
-         Put_Line(File => File,
-                  Item => "      end Initialize;");
+         Put_Line (File => File,
+                   Item => "      procedure Initialize (Object : access "
+                           & Package_Name & ";");
+         Put_Line
+          (File => File,
+           Item => "                            Parent : in Xt.Widget)");
+         Put_Line (File => File,
+                   Item => "      is");
+         Put_Line (File => File,
+                   Item => "      begin");
+         Put_Line (File => File,
+                   Item => "         null;");
+         Put_Line (File => File,
+                   Item => "      end Initialize;");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "      function Create");
-         Put_Line(File => File,
-                  Item => "         Result : "
+         Put_Line (File => File,
+                   Item => "      function Create");
+         Put_Line (File => File,
+                   Item => "         Result : "
                            & Package_Name & "_Access := new " &
                            Package_Name & ";");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "      begin");
-         Put_Line(File => File,
-                  Item => "         Initialize (Result);");
+         Put_Line (File => File,
+                   Item => "      begin");
+         Put_Line (File => File,
+                   Item => "         Initialize (Result);");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "         return Result;");
-         Put_Line(File => File,
-                  Item => "      end Create;");
+         Put_Line (File => File,
+                   Item => "         return Result;");
+         Put_Line (File => File,
+                   Item => "      end Create;");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "   end Constructors;");
+         Put_Line (File => File,
+                   Item => "   end Constructors;");
          New_Line (File);
-         Put_Line(File => File,
-                  Item => "end " & Package_Name & "s;");
+         Put_Line (File => File,
+                   Item => "end " & Package_Name & "s;");
 
       end Generate_Package;
 
@@ -163,11 +165,11 @@ package body Generator.Prototype.Component_Class is
 
    begin
 
-      Create(File => File,
-             Mode => Out_File,
-             Name => "test.ada");
+      Create (File => File,
+              Mode => Out_File,
+              Name => "test.ada");
       Generate_Package (File, Model.Queries.Name_Image (Node));
-      Close(File);
+      Close (File);
    end Generate;
 
 end Generator.Prototype.Component_Class;
