0709b8f826ced7ac53940ec8d88c56d9ee1d6a40
shilov <shilov@661adc5f-87f8-0310-85d6-9a7b5f58d823>
2006-02-14 05:50:25 +0000

    добавлена реализация генератора шаблона пакета

--- trunk/xmada/trunk/source/designer/generator-prototype-component_class.adb
+++ trunk/xmada/trunk/source/designer/generator-prototype-component_class.adb
@@ -36,17 +36,138 @@
 --  $Revision$ $Author$
 --  $Date$
 ------------------------------------------------------------------------------
+with Ada.Wide_Text_IO;
+with Model.Queries;
 
 package body Generator.Prototype.Component_Class is
 
+   use Ada.Wide_Text_IO;
    ---------------------------------------------------------------------------
    --! <Subprogram>
    --!    <Unit> Generate
    --!    <ImplementationNotes>
    ---------------------------------------------------------------------------
    procedure Generate (Node : in Model.Node_Id) is
+
+      procedure Generate_Package (File : File_Type; 
+                                  Package_Name : Wide_String);
+
+      procedure Generate_Package (File : File_Type; 
+                                  Package_Name : Wide_String)
+      is                                 
+      begin
+   --    generation of spec
+
+         Put_Line(File => File,
+                  Item => "package " & Package_Name & "s is");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "   type " & Package_Name & " is limited private;");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "   type " & Package_Name & "_Access is access all " &
+                           Package_Name & ";");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "   package Constructors is");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "      procedure Initialize (Object : access " &
+                           Package_Name & ";");
+         Put_Line(File => File,
+                  Item => "                            Parent : in Xt.Widget);");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "      function Create (Parent : in Xt.Widget)");
+         Put_Line(File => File,
+                  Item => "        return " & Package_Name & "_Access;");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "   end Constructors;");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "private");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "   type " & Package_Name & " is limited record");
+         Put_Line(File => File,
+                  Item => "      null;");
+         Put_Line(File => File,
+                  Item => "   end record;");
+         Put_Line(File => File,
+                  Item => "end " & Package_Name & "s;");
+
+         New_Line (File);
+   --    generation of body
+
+         Put_Line(File => File,
+                  Item => "package body " & Package_Name & "s is");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "   package CallBacks is");
+         Put_Line(File => File,
+                  Item => "      null;");
+         Put_Line(File => File,
+                  Item => "   end CallBacks;");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "   package body CallBacks is");
+         Put_Line(File => File,
+                  Item => "      null;");
+         Put_Line(File => File,
+                  Item => "   end CallBacks;");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "   package body Constructors is");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "      procedure Initialize (Object : access " &
+                           Package_Name & ";");
+         Put_Line(File => File,
+                  Item => "                            Parent : in Xt.Widget)");
+         Put_Line(File => File,
+                  Item => "      is");
+         Put_Line(File => File,
+                  Item => "      begin");
+         Put_Line(File => File,
+                  Item => "         null;");
+         Put_Line(File => File,
+                  Item => "      end Initialize;");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "      function Create");
+         Put_Line(File => File,
+                  Item => "         Result : "
+                           & Package_Name & "_Access := new " &
+                           Package_Name & ";");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "      begin");
+         Put_Line(File => File,
+                  Item => "         Initialize (Result);");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "         return Result;");
+         Put_Line(File => File,
+                  Item => "      end Create;");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "   end Constructors;");
+         New_Line (File);
+         Put_Line(File => File,
+                  Item => "end " & Package_Name & "s;");
+
+      end Generate_Package;
+
+      File : File_Type;
+
    begin
-      null;
+
+      Create(File => File,
+             Mode => Out_File,
+             Name => "test.ada");
+      Generate_Package (File, Model.Queries.Name_Image (Node));
+      Close(File);
    end Generate;
 
 end Generator.Prototype.Component_Class;
